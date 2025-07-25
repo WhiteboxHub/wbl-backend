9c959e6bdede7229151de687f185fd99689ed5b9
Vijay-Animoni <vijayyadav301106@gmail.com>
2025-07-25 16:17:26 -0700

    modified the admin_emails in fapi/main.py

--- fapi/main.py
+++ fapi/main.py
@@ -8,7 +8,7 @@ from  fapi.db import (
     unsubscribe_user, update_user_password ,get_user_by_username, update_user_password ,insert_user,get_google_user_by_email,insert_google_user_db,fetch_candidate_id_by_email,insert_vendor ,fetch_recent_placements , fetch_recent_interviews, get_candidate_by_name, get_candidate_by_id, create_candidate, delete_candidate as db_delete_candidate,update_candidate as db_update_candidate,get_all_placements,
  
 
-    get_placement_by_id,search_placements_by_candidate_name,create_placement,update_placement,delete_placement,get_status,update_status,unsubscribe_lead_user,insert_lead_new
+    get_placement_by_id,search_placements_by_candidate_name,create_placement,update_placement,delete_placement,unsubscribe_lead_user,insert_lead_new
  
   
 )
