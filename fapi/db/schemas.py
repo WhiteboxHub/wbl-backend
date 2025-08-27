594278f25335dbb2f3f9b7c7d440510d73fba2c9
ManisaiReddy <manisaisaduvala21@gmail.com>
2025-08-28 05:29:56 +0530

    updated schemas

--- fapi/db/schemas.py
+++ fapi/db/schemas.py
@@ -651,7 +651,7 @@ class CourseResponse(BaseModel):
     alias: str
     description: Optional[str] = None
     syllabus: Optional[str] = None
-    lastmoddatetime: Optional[datetime] = None
+    #lastmoddatetime: Optional[datetime] = None
 
     model_config = {
         "from_attributes": True
@@ -677,7 +677,7 @@ class SubjectResponse(BaseModel):
     id: int
     name: str
     description: Optional[str] = None
-    lastmoddatetime: Optional[datetime] = None
+    #lastmoddatetime: Optional[datetime] = None
 
     model_config = {
         "from_attributes": True
@@ -696,7 +696,7 @@ class SubjectUpdate(BaseModel):
 class CourseSubjectResponse(BaseModel):
     subject_id: int
     course_id: int
-    lastmoddatetime: Optional[datetime] = None
+    #lastmoddatetime: Optional[datetime] = None
 
     model_config = {
 
