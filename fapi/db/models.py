dc2dfc85a2f4a49db37690e837df01224d305185
Karimulla79 <Pkarimulla9@gmail.com>
2025-09-17 12:05:56 -0700

    Updated all code

--- fapi/db/models.py
+++ fapi/db/models.py
@@ -287,45 +287,6 @@ class CandidateMarketingORM(Base):
         "EmployeeORM", foreign_keys=[marketing_manager], overlaps="marketing_manager_obj"
     )
 # -------------------------------------- Candidate Interview -------------------------------
-
-# class CandidateInterview(Base):
-#     __tablename__ = "candidate_interview"
-
-#     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-#     candidate_id = Column(Integer, ForeignKey("candidate.id"), nullable=False)
-
-#     # Relationship to CandidateORM
-#     candidate = relationship("CandidateORM", back_populates="interviews")
-
-#     company = Column(String(200), nullable=False)
-#     interviewer_emails = Column(Text, nullable=True)
-#     interviewer_contact = Column(Text, nullable=True)
-#     interview_date = Column(Date, nullable=False)
-
-#     mode_of_interview = Column(
-#         Enum("Virtual", "In Person", "Phone", "Assessment", name="mode_of_interview_enum"),
-#         nullable=True
-#     )
-
-#     type_of_interview = Column(
-#         Enum("Assessment", "Recruiter Call", "Technical", "HR Round", "In Person", "Prep Call", 
-#              name="type_of_interview_enum"),
-#         nullable=True
-#     )
-
-#     recording_link = Column(String(500), nullable=True)
-#     backup_url = Column(String(500), nullable=True)
-#     status = Column(String(100), nullable=True)
-
-#     feedback = Column(
-#         Enum("Negative", "Positive", "No Response", "Cancelled", name="feedback_enum"),
-#         nullable=True
-#     )
-
-#     notes = Column(Text, nullable=True)
-#     last_mod_datetime = Column(TIMESTAMP, server_default=func.now(), onupdate=func.now())
-
-
 class CandidateInterview(Base):
     __tablename__ = "candidate_interview"
 
