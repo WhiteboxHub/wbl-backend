2670c6b93c3d9fede6aa63253e6cf4cf6f033e57
Maahi1411 <mahendargoud9866@gmail.com>
2025-09-24 01:41:30 +0530

    added authentication for all the avatar routes

--- fapi/api/routes/user_dashboard.py
+++ fapi/api/routes/user_dashboard.py
@@ -1,16 +1,20 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Security
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 from fapi.utils.user_dashboard_utils import get_current_user
 from fapi.db.models import AuthUserORM
 
 router = APIRouter()
 
+security = HTTPBearer()
+
 @router.get("/user_dashboard")
-def read_user_dashboard(current_user: AuthUserORM = Depends(get_current_user)):
+def read_user_dashboard(
+    current_user: AuthUserORM = Depends(get_current_user),
+    credentials: HTTPAuthorizationCredentials = Security(security),
+):
     return {
         "uname": current_user.uname,
-        # "email": current_user.address,  # change to actual email field if exists
         "full_name": current_user.fullname,
         "phone": current_user.phone,
         "login_count": current_user.logincount,
-        # "last_login": current_user.lastlogin
     }
