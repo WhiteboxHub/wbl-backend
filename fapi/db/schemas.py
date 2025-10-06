2032d84b2d2fc6b036515fe898ebf1ea6c81674a
Rohit <rohithyadav628169@gmail.com>
2025-10-06 14:32:17 -0700

    implemented move_to_prep and mrkt

--- fapi/db/schemas.py
+++ fapi/db/schemas.py
@@ -415,6 +415,7 @@ class CandidatePreparationBase(BaseModel):
     current_topics: Optional[str] = None
     target_date_of_marketing: Optional[date] = None
     notes: Optional[str] = None
+    move_to_mrkt: Optional[bool] = False
 
     candidate: Optional[CandidateBase]  
     instructor1: Optional[EmployeeBase]  
@@ -428,23 +429,24 @@ class CandidatePreparationBase(BaseModel):
 
 
 class CandidatePreparationCreate(CandidatePreparationBase):
-    id: int = Field(..., alias="id")
-    candidate_id: int
-    batch: Optional[str] = None
-    start_date: Optional[date] = None
-    status: str
-    instructor1_id: Optional[int] = Field(None, alias="instructor_1id")
-    instructor2_id: Optional[int] = Field(None, alias="instructor_2id")
-    instructor3_id: Optional[int] = Field(None, alias="instructor_3id")
-    rating: Optional[str] = None
-    tech_rating: Optional[str] = None
-    communication: Optional[str] = None
-    years_of_experience: Optional[str] = None
-    topics_finished: Optional[str] = None
-    current_topics: Optional[str] = None
-    target_date_of_marketing: Optional[date] = None
-    notes: Optional[str] = None
-    candidate: Optional[CandidateBase]  # added line
+    pass
+    # id: int = Field(..., alias="id")
+    # candidate_id: int
+    # batch: Optional[str] = None
+    # start_date: Optional[date] = None
+    # status: str
+    # instructor1_id: Optional[int] = Field(None, alias="instructor_1id")
+    # instructor2_id: Optional[int] = Field(None, alias="instructor_2id")
+    # instructor3_id: Optional[int] = Field(None, alias="instructor_3id")
+    # rating: Optional[str] = None
+    # tech_rating: Optional[str] = None
+    # communication: Optional[str] = None
+    # years_of_experience: Optional[str] = None
+    # topics_finished: Optional[str] = None
+    # current_topics: Optional[str] = None
+    # target_date_of_marketing: Optional[date] = None
+    # notes: Optional[str] = None
+    # candidate: Optional[CandidateBase]  # added line
 
 class CandidatePreparationUpdate(BaseModel):
     batch: Optional[str] = None
@@ -461,6 +463,7 @@ class CandidatePreparationUpdate(BaseModel):
     current_topics: Optional[str] = None
     target_date_of_marketing: Optional[date] = None
     notes: Optional[str] = None
+    move_to_mrkt: Optional[bool] = None
     # candidate: Optional[CandidateBase]  
     
 
@@ -478,6 +481,7 @@ class CandidatePreparationOut(BaseModel):
     target_date_of_marketing: Optional[date] = None
     notes: Optional[str] = None
     last_mod_datetime: Optional[datetime]
+    move_to_mrkt: Optional[bool] = None
 
     candidate: Optional[CandidateBase]
 
