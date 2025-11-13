b3f82452250bc8fe43b76049d56754163e10b16c
sai teja <saiteja001r@gmail.com>
2025-11-13 13:03:58 -0800

    updated main.py

--- fapi/main.py
+++ fapi/main.py
@@ -40,16 +40,10 @@ from fapi.api.routes import (
     vendor, request_demo, unsubscribe,
     user_dashboard, password, employee, course, subject, course_subject,
     course_content, course_material, batch, authuser, avatar_dashboard,
-    session, recording, referrals,candidate_dashboard,email_activity_log,internal_documents, chatbot
-
-
-
-    session, recording, referrals,candidate_dashboard,email_activity_log,internal_documents,linkedin_activity_log
-    session, recording, referrals,candidate_dashboard,email_activity_log,internal_documents, chatbot
-
-
+    session, recording, referrals,candidate_dashboard,email_activity_log,internal_documents, chatbot, linkedin_activity_log,
 
 )
+
 from fapi.utils.permission_gate import enforce_access
 
 def get_db():
@@ -91,7 +85,6 @@ app.include_router(candidate.router, prefix="/api", tags=["Candidates"], depende
 app.include_router(candidate_dashboard.router, tags=["Candidate Dashboard"])
 
 app.include_router(internal_documents.router, prefix="/api/internal-documents", tags=["Internal Documents"])
-app.include_router(chatbot.router, prefix="/api", tags=["Chatbot"])
 # app.include_router(linkedin_activity_log.router, prefix="/api", tags=["LinkedIn Activity Log"], dependencies=[Depends(enforce_access)]) 
 app.include_router(linkedin_activity_log.router, prefix="/api", tags=["LinkedIn Activity Log"])
 
