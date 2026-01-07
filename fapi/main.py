f3729dc38ed0a9a9e6d32d3cb4f619ff26ce92a6
hk <hkinnovapath@gmail.com>
2026-01-07 23:57:40 +0530

    Fix CORS

--- fapi/main.py
+++ fapi/main.py
@@ -19,19 +19,31 @@ app = FastAPI(title="WBL Backend")
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger("wbl")
 
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=[
+#         "https://whitebox-learning.com",
+#         "https://www.whitebox-learning.com",
+#         "http://whitebox-learning.com",
+#         "http://www.whitebox-learning.com",
+#         "http://localhost:3000",
+#         "http://localhost:8000",
+#         "https://wbl-frontend-560359652969.us-central1.run.app",
+#     ],
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "https://whitebox-learning.com",
         "https://www.whitebox-learning.com",
-        "http://whitebox-learning.com",
-        "http://www.whitebox-learning.com",
-        "http://localhost:3000",
-        "http://localhost:8000",
         "https://wbl-frontend-560359652969.us-central1.run.app",
+        "http://localhost:3000",  # dev only
     ],
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["GET", "POST", "OPTIONS"],
     allow_headers=["*"],
 )
 
