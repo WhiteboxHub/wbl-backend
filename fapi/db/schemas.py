d24ca8eda61bd3a41b78d45e5002ad816b45ad47
sampath-velupula <training@whitebox-learning.com>
2026-02-04 04:19:44 +0530

    Update schemas.py

--- fapi/db/schemas.py
+++ fapi/db/schemas.py
@@ -759,6 +759,7 @@ class CandidatePreparationCreate(BaseModel):
     target_date: Optional[date] = None
     notes: Optional[str] = None
     move_to_mrkt: Optional[bool] = False
+    linkedin_id: Optional[str] = None
     github_url: Optional[str] = None
     resume_url: Optional[str] = None
 
@@ -775,6 +776,7 @@ class CandidatePreparationUpdate(BaseModel):
     target_date: Optional[date] = None
     notes: Optional[str] = None
     move_to_mrkt: Optional[bool] = None
+    linkedin_id: Optional[str] = None
     github_url: Optional[str] = None
     resume_url: Optional[str] = None
 
@@ -790,6 +792,7 @@ class CandidatePreparationOut(BaseModel):
     notes: Optional[str] = None
     last_mod_datetime: Optional[datetime] = None
     move_to_mrkt: Optional[bool] = None
+    linkedin_id: Optional[str] = None
     github_url: Optional[str] = None
     resume_url: Optional[str] = None
     is_in_marketing: Optional[str] = "No"
