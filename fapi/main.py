72be89f2a41d758e23329b3c701a4faf5199530f
unknown <rudraprasada0107@gmail.com>
2026-05-21 15:40:58 -0700

    Revert "Remove *  from CORS allow_origins"
    This reverts commit 3cc60b1b5c1e085c7deb620e60e2aee7f41909c1.

--- fapi/main.py
+++ fapi/main.py
@@ -78,7 +78,8 @@ app.add_middleware(
         "http://localhost:3000",
         "http://127.0.0.1:3000",
         "http://localhost:3001",
-        "http://127.0.0.1:8000"
+        "http://127.0.0.1:8000",
+        "*"
     ],
     allow_credentials=True,
     allow_methods=["*"],
