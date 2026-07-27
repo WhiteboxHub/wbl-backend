0a821ece95c5b4953e64e19430d040c59f1d4165
swethabujji <takkoluswethareddy@gmail.com>
2026-07-27 23:37:14 +0530

    Remove fix_db.py

--- fix_db.py
+++
@@ -1,9 +0,0 @@
-import pymysql
-conn = pymysql.connect(host='127.0.0.1', user='root', password='Swetha@123', port=3306)
-cursor = conn.cursor()
-cursor.execute("CREATE USER IF NOT EXISTS 'whitebox-db'@'%' IDENTIFIED BY 'password';")
-cursor.execute("GRANT ALL PRIVILEGES ON *.* TO 'whitebox-db'@'%';")
-cursor.execute("FLUSH PRIVILEGES;")
-conn.commit()
-conn.close()
-print('Fixed definer issue.')
