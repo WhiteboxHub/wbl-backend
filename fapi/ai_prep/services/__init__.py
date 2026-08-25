4e7e014ee3b1810796d7cfc7a460c7f82fc5dbfd
Srimanth <srimanth0312@gmail.com>
2026-08-25 06:38:29 +0530

    backend changes regarding the LLM connection

--- fapi/ai_prep/services/__init__.py
+++ fapi/ai_prep/services/__init__.py
@@ -3,10 +3,15 @@ from fapi.ai_prep.services.assessment_service import start_assessment_session
 from fapi.ai_prep.services.media_service import MediaService
 from fapi.ai_prep.services.report_service import fetch_assessment_report
 from fapi.ai_prep.services.consent_service import record_candidate_consent
+from fapi.ai_prep.services.candidate_llm_key_service import get_candidate_llm_key, CandidateLLMKey
+from fapi.ai_prep.services.llm_client import call_llm
 
 __all__ = [
     "start_assessment_session",
     "MediaService",
     "fetch_assessment_report",
-    "record_candidate_consent"
+    "record_candidate_consent",
+    "get_candidate_llm_key",
+    "CandidateLLMKey",
+    "call_llm",
 ]
